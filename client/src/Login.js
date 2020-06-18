83cca2c9031ba33b92125d3aa921bb61b9d10939
lixuan <lilx@unc.edu>
2020-06-17 21:41:01 -0400

    adding new user route

---
+++ client/src/Login.js
@@ -0,0 +1,81 @@
+import React from 'react';
+import axios from 'axios';
+import {
+    BrowserRouter,
+    Switch,
+    Route,
+    Link
+  } from "react-router-dom";
+
+
+class Login extends React.Component {
+  render() {
+    return (      
+      <BrowserRouter>
+        <div>
+            <Switch>
+              {/* Add routes here */}
+             <Route path="/" component={Home} exact/>
+      
+           </Switch>
+        </div> 
+      </BrowserRouter>
+    );
+  }
+}
+
+class Home extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleClick = this.handleClick.bind(this);
+    this.state = { 
+      firstname: '',
+      lastname: '',
+      email: ''  
+    }
+  }
+
+
+  handleClick() {
+    console.log('Button has been pressed')
+    
+    axios.post(`/POST/api/users`, this.state)
+      .then(req  => {
+        if (req.data.status === 'success'){
+          console.log("Data sent"); 
+          this.resetForm()
+        }else if(req.data.status === 'fail'){
+          console.log("it failed m8");
+        }
+      })
+  }
+
+
+  render() {
+    return (
+
+      <div>
+        <h2>Social Circle or whatever the name is</h2>
+        <h5>Some sort of explanation for how the app works</h5>
+
+        <form>
+          <label htmlFor="firstname">First Name</label>
+          <input id="firstname" name="firstname" type="text" />
+
+          <label htmlFor="lastname">Last Name</label>
+          <input id="lastname" name="lastname" type="text" />
+
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type="email" />
+
+          <button onClick={this.handleClick}>Submit or die!</button>
+        </form>
+      </div>
+
+    )
+  }
+}
+
+
+
+export default Login;
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ var router = express.Router()
 var db = require('../db/index.js')
 
 
+
+// Route to get sql data
 router.get('/test', function(req,res) {
     const query = 'Select * from users';
     db.query(query)
@@ -10,5 +12,36 @@ router.get('/test', function(req,res) {
         res.send(results.rows);
     })
 })
+
+// Route for login/adding new user
+router.post('/POST/api/users', function(req,res){
+    // const values = [req.body.firstname,
+    //                 req.body.lastname,
+    //                 req.body.email]
+
+    // db.query(`INSERT INTO users(firstname, lastname, email)
+    //           VALUES($1, $2, $3)`, values,
+    //           (q_err, q_res) => {
+    //             res.json(q_res.rows)
+    //   })
+
+    const { firstname, lastname, email } = req.body
+
+    pool.query('INSERT INTO users (firstname, lastname, email) VALUES ($1, $2, $3)', [firstname, lastname, email], (error, results) => {
+        if (error) {
+        throw error
+        }
+        response.status(201).send(`User added with ID: ${result.insertId}`)
+  })
+})
+
+app.post('/users', (request, response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if (error) throw error;
+ 
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+
 
 module.exports = router
