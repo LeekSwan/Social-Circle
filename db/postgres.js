0f2ef15638a631c77903e4c00352305adbd36b80
jasontyu <jasonyu6154@gmail.com>
2020-06-18 16:55:57 -0400

    Configure production db

--- db/postgres.js
+++ db/postgres.js
@@ -1,12 +1,25 @@
 const { Client } = require('pg');
 
-const client = new Client({
-    user: process.env.PG_LOCAL_USER,
-    host: process.env.PG_LOCAL_HOST,
-    database: process.env.PG_LOCAL_DATABASE,
-    port: process.env.PG_LOCAL_PORT
-});
+let db_params = {}
+if (process.env.NODE_ENV === 'production') {
+    db_params = {
+        connectionString: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        }
+    };
+} else if (process.env.NODE_ENV === 'dev') {
+    db_params = {
+        user: process.env.PG_LOCAL_USER,
+        host: process.env.PG_LOCAL_HOST,
+        database: process.env.PG_LOCAL_DATABASE,
+        port: process.env.PG_LOCAL_PORT
+    }
+} else {
+    console.log('Unrecognized NODE_ENV: ', process.env.NODE_ENV);
+}
 
+const client = new Client(db_params);
 client.connect();
 
 function query(text) {
