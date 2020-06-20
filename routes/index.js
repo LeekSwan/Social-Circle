9c311f0d91b3e4d33052e706bf3bcca7eab59ae0
jasontyu <jasonyu6154@gmail.com>
2020-06-19 21:07:31 -0400

    Add simple test response for home route /

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,9 @@ var express = require('express')
 var router = express.Router()
 var db = require('../db/index.js')
 
+router.get('/', function (req,res) {
+    res.send('works for me');
+})
 
 router.get('/test', function(req,res) {
     const query = 'Select * from users';
